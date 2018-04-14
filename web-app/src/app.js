dedf47be36db5750022d9f40b2a7a04fe928bded
Ismael Balafrej <ismael.balafrej@usherbrooke.ca>
2018-04-14 14:27:54 -0400

    Added firebase hosting

---
+++ web-app/src/app.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBsNLNZeJ98FPip3vmaQkZVF8Llu_nMJN4",
+  authDomain: "wao-hackdon.firebaseapp.com",
+  databaseURL: "https://wao-hackdon.firebaseio.com",
+  projectId: "wao-hackdon",
+  storageBucket: "",
+  messagingSenderId: "879951076825"
+};
+firebase.initializeApp(config);
+
