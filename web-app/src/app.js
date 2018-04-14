ac3974a3346f95c991380f320688c337b6a1fe0a
Ismael Balafrej <ismael.balafrej@usherbrooke.ca>
2018-04-14 16:21:13 -0400

    Added navigation

--- web-app/src/app.js
+++ web-app/src/app.js
@@ -9,45 +9,78 @@ var config = {
 };
 firebase.initializeApp(config);
 
-firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL).then(function () {
 
-  var provider = new firebase.auth.FacebookAuthProvider();
-  provider.addScope('user_friends');
-  provider.addScope('user_location');
-  provider.setCustomParameters({
-    'display': 'popup'
-  });
+function auth() {
+  firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL).then(function () {
 
-  firebase.auth().signInWithPopup(provider).then(function (result) {
-    var token = result.credential.accessToken; //refreshToken ?
-    var user = result.user;
-    var ref = firebase.database().ref("/user").child(user.uid);
-
-    user = {
-      uid: user.uid,
-      displayName: user.displayName,
-      photoURL: user.photoURL,
-      email: user.email,
-      facebookUid: user.providerData[0].uid
-    };
-    ref.update(user);
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-        ref.child("friends").update(JSON.parse(xhttp.responseText));
+    var provider = new firebase.auth.FacebookAuthProvider();
+    provider.addScope('user_friends');
+    provider.addScope('user_location');
+    provider.setCustomParameters({
+      'display': 'popup'
+    });
+  
+    firebase.auth().getRedirectResult().then(function(result) {
+      if (!result.user) {
+        firebase.auth().signInWithPopup(provider).then(function (result) {
+          var token = result.credential.accessToken; //refreshToken ?
+          var user = result.user;
+          var ref = firebase.database().ref("/user").child(user.uid);
+          user = {
+            uid: user.uid,
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+            email: user.email,
+            facebookUid: user.providerData[0].uid
+          };
+          ref.update(user);
+          var xhttp = new XMLHttpRequest();
+          xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+              ref.child("friends").update(JSON.parse(xhttp.responseText));
+            }
+          };
+          xhttp.open("GET", `https://graph.facebook.com/v2.12/${user.facebookUid}/friends?access_token=${token}`, true);
+          xhttp.send();
+      
+        }).catch(function (error) {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          var email = error.email;
+          var credential = error.credential;
+          console.error(errorMessage);
+          //"uid", "displayName", "photoURL", "email"
+        });
       }
-    };
-    xhttp.open("GET", `https://graph.facebook.com/v2.12/${user.facebookUid}/friends?access_token=${token}`, true);
-    xhttp.send();
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      console.error(errorMessage);
+      
+    });
+  })
+  
+  
+}
 
-  }).catch(function (error) {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    var email = error.email;
-    var credential = error.credential;
-    console.error(errorMessage);
-    //"uid", "displayName", "photoURL", "email"
-  });
+//auth();
+
+$("#app-btn-accueil").on("click", function() {
+  $(".app-widget").hide();
+  $("#app-container-accueil").show();
 })
 
+$("#app-btn-dons").on("click", function() {
+  $(".app-widget").hide();
+  $("#app-container-dons").show();
+})
 
+$("#app-btn-fondation").on("click", function() {
+  $(".app-widget").hide();
+  $("#app-container-fondation").show();
+})
