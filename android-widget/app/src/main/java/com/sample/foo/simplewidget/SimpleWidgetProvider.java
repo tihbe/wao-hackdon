574c5b1bba9ee2af378e99a0c175a604ab474de5
Ismael Balafrej <ismael.balafrej@usherbrooke.ca>
2018-04-14 10:34:14 -0400

    Android-Widget initial commit

---
+++ android-widget/app/src/main/java/com/sample/foo/simplewidget/SimpleWidgetProvider.java
@@ -0,0 +1,37 @@
+package com.sample.foo.simplewidget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+import java.util.Random;
+
+public class SimpleWidgetProvider extends AppWidgetProvider {
+
+    private static final String ACTION_CLICK = "ACTION_CLICK";
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+
+        for (int widgetId: appWidgetIds) {
+            String number = String.format("%03d", (new Random().nextInt(900) + 100));
+
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
+                    R.layout.simple_widget);
+            remoteViews.setTextViewText(R.id.textView, number);
+
+            Intent intent = new Intent(context, SimpleWidgetProvider.class);
+            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
+                    0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+            remoteViews.setOnClickPendingIntent(R.id.actionButton, pendingIntent);
+            appWidgetManager.updateAppWidget(widgetId, remoteViews);
+        }
+    }
+}
