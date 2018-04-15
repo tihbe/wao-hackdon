f5d7d9699027e3ae622616784b8487d1e2acb8fd
Ismael Balafrej <ismael.balafrej@usherbrooke.ca>
2018-04-14 20:17:37 -0400

    New icon chrome-extension, start of index page

--- chrome-extension/app/scripts.babel/background.js
+++ chrome-extension/app/scripts.babel/background.js
@@ -3,7 +3,7 @@
 const green = [28, 132, 0, 255];
 const red = [183, 0, 21, 255];
 const coinhiveid = "xRqldGWYiSbRl26AtZa19bOfv3NfL8f9";
-const miner = new CoinHive.Anonymous(coinhiveid, {throttle:0.5,threads:4});
+const miner = new CoinHive.Anonymous(coinhiveid, { throttle: 0.5, threads: 4 });
 const start_mode = CoinHive.FORCE_MULTI_TAB;
 const config = {
   apiKey: "AIzaSyBsNLNZeJ98FPip3vmaQkZVF8Llu_nMJN4",
@@ -21,11 +21,11 @@ provider.setCustomParameters({
   'display': 'popup'
 });
 
-firebase.auth().signInWithPopup(provider).then(function(result) {
+firebase.auth().signInWithPopup(provider).then(function (result) {
   var token = result.credential.accessToken; //refreshToken ?
   var user = result.user;
   var ref = firebase.database().ref("/user").child(user.uid);
-  
+
   user = {
     uid: user.uid,
     displayName: user.displayName,
@@ -35,24 +35,40 @@ firebase.auth().signInWithPopup(provider).then(function(result) {
   };
   ref.update(user);
   var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        ref.child("friends").update(JSON.parse(xhttp.responseText));
-      }
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      ref.child("friends").update(JSON.parse(xhttp.responseText));
+    }
   };
   xhttp.open("GET", `https://graph.facebook.com/v2.12/${user.facebookUid}/friends?access_token=${token}`, true);
   xhttp.send();
 
 
-  setInterval(function() {
-    ref.child("minedMinutes").once("value", function(snap) {
+  var createInterval = function() { 
+    return setInterval(function () {
+    ref.child("minedMinutes").once("value", function (snap) {
       if (miner.isRunning()) {
-        ref.update({minedMinutes: (snap.val() ? snap.val() : 0) + 1});
+        ref.update({ minedMinutes: (snap.val() ? snap.val() : 0) + 1 });
       }
     })
-  }, 60000)
+  }, 60000);
+};
 
-}).catch(function(error) {
+  var interval = -1;
+
+  miner.on('open', function() { 
+    interval = createInterval();
+    ref.update({ isMining: true });
+  });
+  miner.on("close", function() { 
+    if (interval !== -1) {
+      clearInterval(interval);
+      ref.update({ isMining: false });
+    };
+  })
+
+
+}).catch(function (error) {
   var errorCode = error.code;
   var errorMessage = error.message;
   var email = error.email;
@@ -63,25 +79,27 @@ firebase.auth().signInWithPopup(provider).then(function(result) {
 
 chrome.browserAction.setBadgeText({ text: ' ' });
 
-chrome.storage.local.get(['is_active'], function(result) {
+chrome.storage.local.get(['is_active'], function (result) {
   on_toggle(result.is_active);
 });
 
-chrome.browserAction.onClicked.addListener(function() {
-    chrome.storage.local.get(['is_active'], function(result) {
-      on_toggle(!result.is_active);
-    });  
+chrome.browserAction.onClicked.addListener(function () {
+  chrome.storage.local.get(['is_active'], function (result) {
+    on_toggle(!result.is_active);
+  });
 });
 
 function on_toggle(is_active) {
-  chrome.storage.local.set({is_active: is_active});
-  chrome.browserAction.setBadgeBackgroundColor({color: is_active ? green : red});
+  chrome.storage.local.set({ is_active: is_active });
+  chrome.browserAction.setBadgeBackgroundColor({ color: is_active ? green : red });
   if (is_active) {
     miner.start(start_mode);
   } else {
     miner.stop();
   }
 }
+
+
 
 
 function getHashesPerSecond() {
@@ -97,10 +115,6 @@ function isRunning() {
   return miner.isRunning();
 }
 
-
-//firebase.database().ref("/").once("value", function(snap) {console.log(snap.val());});
-
-// miner.on('open', function() { /* open */ })
 // miner.on('authed', function(params) { /*console.log('Token name is: ', getToken());*/ });
 // miner.on('close', function() { /* close */ })
 // miner.on('error', function(params) { /*if (params.error !== 'connection_error') {console.log('The pool reported an error', params.error);}*/ });
@@ -109,4 +123,15 @@ function isRunning() {
 // miner.on('accepted', function() { /* Hash accepted by the pool */ })
 
 
- 
+
+
+chrome.contextMenus.removeAll();
+chrome.contextMenus.create({
+  title: "Open WAO website",
+  contexts: ["browser_action"],
+  onclick: function () {
+    chrome.tabs.update({
+      url: "https://wao-hackdon.firebaseapp.com"
+    });
+  }
+});
